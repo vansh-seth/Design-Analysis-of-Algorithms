90b5f132fa5ddbc762f9d757d939345ad657221f
vansh-seth <111755254+vansh-seth@users.noreply.github.com>
2024-02-20 10:02:07 +0530

    Create Heap-sort.c

---
+++ Practicals/Practical-6-(b)/Heap-sort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <time.h>
+void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+  
+void heapify(int arr[], int n, int i) 
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+    if (largest != i) 
+    {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+    }
+}
+void heapSort(int arr[], int n) 
+{
+    clock_t start = clock();
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+    for (int i = n - 1; i >= 0; i--) 
+    {
+      swap(&arr[0], &arr[i]);
+      heapify(arr, i, 0);
+    }
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", time_taken);
+}
+void printArray(int arr[], int n) 
+{
+    for (int i = 0; i < n; ++i)
+      printf("%d ", arr[i]);
+    printf("\n");
+}
+int main() 
+{
+    int arr[] = {1, 12, 9, 5, 6, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+  
+    heapSort(arr, n);
+  
+    printf("Sorted array is \n");
+    printArray(arr, n);
+}
