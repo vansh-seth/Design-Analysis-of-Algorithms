abda5b99711b5fdaddee2a940cef0c0118c29eff
vansh-seth <111755254+vansh-seth@users.noreply.github.com>
2024-03-05 09:59:22 +0530

    Update Fibbonaci.c

--- Practicals/Practical-7/Fibbonaci.c
+++ Practicals/Practical-7/Fibbonaci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 void multiply(int F[2][2], int M[2][2]) {
     int x =  F[0][0]*M[0][0] + F[0][1]*M[1][0];
     int y =  F[0][0]*M[0][1] + F[0][1]*M[1][1];
@@ -30,12 +31,24 @@ int fib(int n) {
 
 int main() {
     int n;
+    clock_t start, end;
+    double cpu_time_used;
+
     printf("Enter the value of n: ");
     scanf("%d", &n);
+
+    start = clock(); 
+
     printf("The Fibonacci sequence up to the %dth number is:\n", n);
     for (int i = 1; i <= n; i++) {
         printf("%d ", fib(i));
     }
     printf("\n");
+
+    end = clock(); 
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Time taken to compute the Fibonacci sequence: %f seconds\n", cpu_time_used);
+
     return 0;
 }
